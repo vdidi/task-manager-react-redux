b938b09b38d4aa55b21c16cd4713d652ba7499de
vdidi <vitor_hugo_di@hotmail.com>
2020-06-29 08:43:07 -0300

    integra método adicionar com o backend

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -6,7 +6,9 @@ export default props => (
   <div role='form' className='todoForm'>
     <Grid cols='12 9 10'>
       <input id='description' className='form-control'
-        placeholder='Adicione uma tarefa'></input>
+        placeholder='Adicione uma tarefa'
+        onChange={props.handleChange}
+        value={props.description}></input>
     </Grid>
 
     <Grid cols='12 3 2'>
