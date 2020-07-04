8f002c4787ed80902dc10dcff1958c9df928b94f
vdidi <vitor_hugo_di@hotmail.com>
2020-07-04 10:01:47 -0300

    adiciona filtro de busca por nome da task

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -17,12 +17,18 @@ export default class Todo extends Component {
     this.handleRemove = this.handleRemove.bind(this)
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
     this.refresh()
   }
 
-  refresh() {
-    axios.get(`${URL}?sort=-creaatAt`)
-      .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+  refresh(description = '') {
+    const search = description ? `&description__regex=/${description}/` : '' 
+    axios.get(`${URL}?sort=-creaatAt${search}`)
+      .then(resp => this.setState({...this.state, description, list: resp.data}))
+  }
+
+  handleSearch() {
+    this.refresh(this.state.description);
   }
 
   handleChange(e) {
@@ -37,17 +43,17 @@ export default class Todo extends Component {
 
   handleRemove(todo) {
     axios.delete(`${URL}/${todo._id}`)
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   }
 
   handleMarkAsDone(todo) {
     axios.put(`${URL}/${todo._id}`, {...todo, done: true })
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   } 
 
   handleMarkAsPending(todo) {
     axios.put(`${URL}/${todo._id}`, {...todo, done: false })
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   }
 
   render () {
@@ -57,7 +63,8 @@ export default class Todo extends Component {
         <TodoForm 
           description={this.state.description} 
           handleChange={this.handleChange}
-          handleAdd={this.handleAdd} />
+          handleAdd={this.handleAdd}
+          handleSearch={this.handleSearch}/>
         <TodoList 
           list={this.state.list} 
           handleRemove={this.handleRemove}
