ebef1ffc401d449bfe38fb947dc37c712c066e83
vdidi <vitor_hugo_di@hotmail.com>
2020-07-04 10:08:52 -0300

    adiciona limpar/resetar campo de busca

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -18,6 +18,8 @@ export default class Todo extends Component {
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
     this.handleSearch = this.handleSearch.bind(this)
+    this.handleClear = this.handleClear.bind(this)
+
     this.refresh()
   }
 
@@ -56,6 +58,10 @@ export default class Todo extends Component {
       .then(resp => this.refresh(this.state.description))
   }
 
+  handleClear() {
+    this.refresh()
+  }
+
   render () {
     return (
       <div>
@@ -64,7 +70,8 @@ export default class Todo extends Component {
           description={this.state.description} 
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
-          handleSearch={this.handleSearch}/>
+          handleSearch={this.handleSearch}
+          handleClear={this.handleClear}/>
         <TodoList 
           list={this.state.list} 
           handleRemove={this.handleRemove}
