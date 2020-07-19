e10bc20cf8b06b8a6997f1a2691299b5226a4cc8
vdidi <vitor_hugo_di@hotmail.com>
2020-07-19 15:05:16 -0300

    conect todolist com o redux

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import IconButton from '../template/iconButton'
+import { connect } from 'react-redux'
 
-export default props => {
+const TodoList = props => {
 
   const renderRows = () => {
     const list = props.list || []
@@ -33,4 +34,8 @@ export default props => {
       </tbody>
     </table>
   )
-  }
+}
+
+const mapStateToProps = state => ({list: state.todo.list})
+
+export default connect(mapStateToProps)(TodoList)
